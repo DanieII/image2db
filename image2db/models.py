35b72347fc53cbf57290b45ee2990e615cbdd0a1
DanieII <danielbogdanov1748@gmail.com>
2025-08-07 17:59:32 +0300

    Remove unnecessary comment from Record model class

--- image2db/models.py
+++ image2db/models.py
@@ -2,16 +2,6 @@ from pydantic import BaseModel
 
 
 class Record(BaseModel):
-    """
-    Define your data structure by inheriting from this class.
-
-    Example:
-        class Record(BaseModel):
-            invoice_number: str
-            amount: float
-            date: str
-    """
-
     pass
 
 
